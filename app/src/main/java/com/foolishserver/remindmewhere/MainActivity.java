15738c976958b27051cfecff08ffddd96bbea388
ZWilliams <zackwilliams96@outlook.com>
2017-01-02 22:30:32 -0600

    Google Auth

--- app/src/main/java/com/foolishserver/remindmewhere/MainActivity.java
+++ app/src/main/java/com/foolishserver/remindmewhere/MainActivity.java
@@ -1,82 +1,123 @@
 package com.foolishserver.remindmewhere;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import java.util.Random;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
+import static com.foolishserver.remindmewhere.R.id.get;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Button b_get;
+    private Button b_signout;
     private GPSTracker gps;
     double longitude;
     double latitude;
     private TextView tlat;
     private TextView tlong;
     private DatabaseReference mDatabase;
+    private FirebaseUser mFirebaseUser;
+    final Context context = this;
+
+    private String mUserId;
+    private FirebaseAuth mFirebaseAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b_get = (Button)findViewById(R.id.get);
-        tlat = (TextView)findViewById(R.id.show_latitude);
-        tlong = (TextView)findViewById(R.id.show_longitude);
+
+
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
 
 
-
-        b_get.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                gps = new GPSTracker(MainActivity.this);
-
-
-                if(gps.canGetLocation()){
+        if (mFirebaseUser == null) {
+            // Not logged in, launch the Log In activity
+            loadLogInView();
+        } else {
+            mUserId = mFirebaseUser.getUid();
 
 
-                    longitude = gps.getLongitude();
-                    latitude = gps .getLatitude();
+            b_get = (Button) findViewById(get);
+            tlat = (TextView) findViewById(R.id.show_latitude);
+            tlong = (TextView) findViewById(R.id.show_longitude);
+            mDatabase = FirebaseDatabase.getInstance().getReference();
+            b_signout = (Button) findViewById(R.id.sign_out);
 
-                    String longstring = Double.toString(longitude);
-                    String latstring = Double.toString(latitude);
+
+            //Get Location and send to server
+            b_get.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    gps = new GPSTracker(MainActivity.this);
 
 
-                    tlat.setText(latstring);
-                    tlong.setText(longstring);
+                    if (gps.canGetLocation()) {
 
+
+                        longitude = gps.getLongitude();
+                        latitude = gps.getLatitude();
+
+                        String longstring = Double.toString(longitude);
+                        String latstring = Double.toString(latitude);
+
+
+                        tlat.setText(latstring);
+                        tlong.setText(longstring);
+
+                    } else {
+                        tlat.setText("Sorry no latitude");
+                        tlong.setText("Sorry no longitude");
+                    }
+
+
+                    mDatabase.child(mUserId).child("Longitude").setValue(longitude);
+                    mDatabase.child(mUserId).child("Latitude").setValue(latitude);
+                    mDatabase.child(mUserId).child("Time").setValue(System.currentTimeMillis());
+                    mDatabase.child(mUserId).child("Email").setValue(mFirebaseUser.getEmail());
                 }
-                else
-                {
-                    tlat.setText("Sorry no latitude");
-                    tlong.setText("Sorry no longitude");
-                }
-
-
-                mDatabase.child("z-android").child("Longitude").setValue(longitude);
-                mDatabase.child("z-android").child("Latitude").setValue(latitude);
-                mDatabase.child("z-android").child("Time").setValue(System.currentTimeMillis());
-            }
-        });
-    }
+            });
+        }
 
     /*@Override
     protected void onDestroy() {
         super.onDestroy();
         gps.stopUsingGPS();
     }
-   */ @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        setContentView(R.layout.activity_main);
+   */
     }
+    private void loadLogInView() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+    public void signOut(View view){
+        Intent intent = new Intent (this, GoogleSignIn.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+
+    }
+
 
 
 }
